26a8eb42f97b86fd7cf2719f527da73df058fb01
Souji 2613 <puppypoola@gmail.com>
2018-06-25 16:10:16 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,12 +2,12 @@
 def display_board
  
   board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-   puts "A Tic Tac Toe Board"
-  puts " #{board[0]} | #{board[1]} | #{board[2]}"
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]}"
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]}"
+   puts " A Tic Tac Toe Board "
+   puts " #{board[0]} | #{board[1]} | #{board[2]}"
+   puts "-----------"
+   puts " #{board[3]} | #{board[4]} | #{board[5]}"
+   puts "-----------"
+   puts " #{board[6]} | #{board[7]} | #{board[8]}"
 end
 display_board
   
